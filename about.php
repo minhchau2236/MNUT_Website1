45c5a751a22c8c7b21676f858d3abb59f216d769
minhchau2236 <minhchau2236@gmail.com>
2017-02-16 00:20:56 +0700

    11_create_about_page

---
+++ about.php
@@ -0,0 +1,50 @@
+
+<?php
+/* Template Name: about template*/get_header();
+ ?>
+<!-- start content-wrapper -->
+		<div class="content-wrapper width-common"> 
+			<div id="page-header">
+				<div class="container">
+					<ul class="breadcrumb">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li class="active">About Torsten Seeger</li>
+					</ul>
+				</div>
+			</div>
+			<!-- #page-header -->
+			
+			<div class="container about-wrapper">	
+				<div class="row">
+					<div class="divide40"></div>
+					<div class="col-xs-12 col-sm-12 col-md-6">
+						<div class="divide30"></div>
+						<figure> <img class="img-responsive" src="<?php bloginfo('template_url') ?>/resources/images/about.png" alt=""> </figure>
+					</div>
+					<div class="col-xs-12 col-sm-12 col-md-6">
+						<h1 class="text-header">Dr. Torsten seeger</h1>						
+						
+						<p>Born in 1970 in Brandenburg region in Germany Torsten spend a large part of his life to learn and build a career as a scientist and manager at high school, university and in an international corporation. </p> 
+						
+						
+						<p>At the age of 43 he experienced his first family constellation facilitated by Monika Wyss in Kuala Lumpur/ Malaysia. This convinced him.</p>
+						
+						
+						<p>He decided to become a facilitator for family constellation under Monika‘s guidance. Later on, he was trained by Rolf Lutterbeck in Integral Organisational Structure Constellation (IOSA).</p>
+						
+						
+						<p>In January 2017 Torsten founded ConsulToSee in Ho Chi Minh City/Vietnam with the goal to introduce Systemic Constellation in Vietnam.</p>
+					</div>
+					
+				</div>	
+				
+				<div class="divide80"></div>
+			</div>
+		</div>
+		<!-- end content-wrapper -->
+    
+<?php
+/*Main template file*/
+get_footer();
+?>
