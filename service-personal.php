e449855ab53ffd096710617d0d04fb2d5e2566c0
minhchau2236 <minhchau2236@gmail.com>
2017-02-18 00:59:16 +0700

    tasks/15_Create_services_pages

---
+++ service-personal.php
@@ -0,0 +1,123 @@
+<?php
+/* Template Name: service-personal template*/get_header();
+ ?>
+<!-- start content-wrapper -->
+		<div class="content-wrapper width-common"> 
+			<div id="page-header">
+				<div class="container">
+					<ul class="breadcrumb">
+						<li><a href="#">Home</a></li>						
+						<li class="active">Personal developement Consulting </li>
+					</ul>
+				</div>
+			</div>
+			<!-- #page-header -->
+			
+			<div class="container">
+				<h1 class="text-header text-center">Personal developement Consulting </h1>
+				<div class="summary">
+					<p class="text-center"><i>Reach us at info@consultosee.com or +84 163 969 1059 for a free personal consulting</i></p>
+					<p class="text-center"><i>Using the powerful method of systemic constellation, we support you in</i></p>
+				</div>
+				
+				<div class="divide20"></div>
+							
+				<div class="row wow fadeIn" data-wow-duration="1s" data-wow-delay="0.3s">
+					<div class="col-sm-12">
+						<div class="feature feature-s">
+							<div class="icon icon-s"> <img src="<?php bloginfo('template_url') ?>/resources/images/icon-06.png" alt="" /> </div>
+							<h4>Improve the relationships with our parents</h4>
+							<p>The relationship with our parents is not always easy. But our parents are our roots.  With systemic constellation we acknowledge them as they are, live our own life freely and in the certainty of their support</p>
+						</div>
+						<!--/.feature --> 
+					</div>
+					<!--/column -->													
+				</div>
+				<!--/.row --> 
+				<div class="divide20"></div>
+				<div class="row wow fadeIn" data-wow-duration="1s" data-wow-delay="0.3s">
+					<div class="col-sm-6">
+						<div class="feature feature-s">
+							<div class="icon icon-s"> <img src="<?php bloginfo('template_url') ?>/resources/images/icon-07.png" alt="" /> </div>
+							<h4>Problem Solving</h4>
+							<p>See problem as something that helps you to grow, as something that shows you your strengths and qualities. </p>
+						</div>
+						<!--/.feature --> 
+					</div>
+					<!--/column -->
+					
+					<div class="col-sm-6">
+						<div class="feature feature-s">
+							<div class="icon icon-s"> <img src="<?php bloginfo('template_url') ?>/resources/images/icon-08.png" alt="" /> </div>
+							<h4>Work on Limiting Beliefs</h4>
+							<p>Limiting Beliefs beliefs are stopping us from reaching happiness and sucess. Look at these belief and leave them</p>
+						</div>
+						<!--/.feature --> 
+					</div>
+					<!--/column --> 
+					
+				</div>
+				<!--/.row -->
+				<div class="divide20"></div>
+				
+				<div class="row wow fadeIn" data-wow-duration="1s" data-wow-delay="0.6s">
+					<div class="col-sm-6">
+						<div class="feature feature-s">
+							<div class="icon icon-s"> <img src="<?php bloginfo('template_url') ?>/resources/images/icon-09.png" alt="" /> </div>
+							<h4>Identify and Break free of your Patterns</h4>
+							<p>Why  the same problems happen to you again and again. Have a look at what behind the pattern</p>
+						</div>
+						<!--/.feature --> 
+					</div>
+					<!--/column -->
+				
+					<div class="col-sm-6">
+						<div class="feature feature-s">
+							<div class="icon icon-s"> <img src="<?php bloginfo('template_url') ?>/resources/images/icon-10.png" alt="" /> </div>
+							<h4>Decsion Taking</h4>
+							<p>Need to take a decision but for some reason you can not do it. Help you to become more clear about the options and brings you to a conclusion in a natural and intuitive way. </p>
+						</div>
+						<!--/.feature --> 
+					</div>
+					<!--/column --> 							
+				</div>
+				<!--/.row -->
+	
+				<div class="divide60"></div>
+				<div class="row wow fadeIn" data-wow-duration="1s" data-wow-delay="1.0s">
+					<div class="col-md-4 col-md-offset-2 col-sm-12 col-xs-12 text-center wow fadeIn position-relative" data-wow-duration="1s">
+						<div class="quote style-1 bg-quote-right">
+							<blockquote>
+								<p class="quote-text">&ldquo; 
+									To be adult does not mean to do what you are supposed to do!
+									To be adult means to follow your heart!
+								&rdquo;</p>
+								<p>Wilfred Nelles</p>
+							</blockquote>
+						</div>
+						<figure class="img-top"> <img src="<?php bloginfo('template_url') ?>/resources/images/object-2.png" alt="" /> </figure>							
+					</div>
+					<div class="col-md-6 col-sm-12 col-xs-12">
+						<div class="divide50"></div>
+						<h2 class="text-header">Get to know each other</h2>
+						<p>If you’re still unsure. How about getting to know each other first to see if it’s right. For a free personal consulting, reach us at:</p>
+						<div class="divide10"></div>
+						<ul class="contact">
+							<li><i class="mdi mdi-phone"></i>+ 84 163 969 1059 </li>
+							<li><i class="mdi mdi-email"></i>info@consultosee.com</li>							
+						</ul>
+						 
+						<h2 class="text-header">or experience it yourself at</h2>
+						<a href="#" class="btn btn-transparent btn-large">Our workshop</a>
+					</div>
+				</div>
+				
+				<div class="divide80"></div>
+			</div>
+		</div>
+		<!-- end content-wrapper -->
+
+<?php
+/*Main template file*/
+get_footer();
+?>
---
+++ service.php
@@ -0,0 +1,136 @@
+
+<?php
+/* Template Name: service template*/get_header();
+ ?>
+<!-- start content-wrapper -->
+		<div class="content-wrapper width-common"> 
+			<div id="page-header">
+				<div class="container">
+					<ul class="breadcrumb">
+						<li><a href="#">Home</a></li>						
+						<li class="active">Management Consulting</li>
+					</ul>
+				</div>
+			</div>
+			<!-- #page-header -->
+			
+			<div class="container">
+				<h1 class="text-header text-center">Management Consulting</h1>
+				<div class="summary">
+					<p class="text-center"><i>Reach us at info@consultosee.com or +84 163 969 1059 for a free personal consulting</i></p>
+					<p class="text-center"><i>Using the powerful method of systemic constellation, we support you in</i></p>
+				</div>
+				
+				<div class="divide40"></div>
+				<div class="row wow fadeIn" data-wow-duration="1s" data-wow-delay="0.3s">
+					<div class="col-sm-6">
+						<div class="feature feature-s">
+							<div class="icon icon-s"> <img src="<?php bloginfo('template_url') ?>/resources/images/icon-01.png" alt="" /> </div>
+							<h4>Change Management Process</h4>
+							<p>Are you planning an important step in your business? What pushes you to change and what is stopping you</p>
+							<p>With systemic constellation you get a new perspective on what pushes you to change and what is stopping you.</p>
+						</div>
+						<!--/.feature --> 
+					</div>
+					<!--/column -->
+					
+					<div class="col-sm-6">
+						<div class="feature feature-s">
+							<div class="icon icon-s"> <img src="<?php bloginfo('template_url') ?>/resources/images/icon-02.png" alt="" /> </div>
+							<h4>Strategy Review and Development</h4>
+							<p>You are developing or have developed a strategy to reach a certain goal.</p>
+							<p>With systemic constellation we can analyse how effective the different measures of your strategy paper are. In addition we can simulate a modified plan.</p>
+						</div>
+						<!--/.feature --> 
+					</div>
+					<!--/column --> 
+					
+				</div>
+				<!--/.row -->
+				
+				<div class="divide20"></div>				
+				<div class="row wow fadeIn" data-wow-duration="1s" data-wow-delay="0.6s">
+					<div class="col-sm-6">
+						<div class="feature feature-s">
+							<div class="icon icon-s"> <img src="<?php bloginfo('template_url') ?>/resources/images/icon-03.png" alt="" /> </div>
+							<h4>Market Research</h4>
+							<p>You want to know how your product is received by different customers or you are planning to launch a new product. </p>
+							<p>With systemic constellation you get a direct insight into the perspective of your customers.</p>
+						</div>
+						<!--/.feature --> 
+					</div>
+					<!--/column -->
+				
+					<div class="col-sm-6">
+						<div class="feature feature-s">
+							<div class="icon icon-s"> <img src="<?php bloginfo('template_url') ?>/resources/images/icon-04.png" alt="" /> </div>
+							<h4>Project Management</h4>
+							<p>Seting up and Improving your collaboratiion with partners of various cultural backgrounds and different company values </p>
+						</div>
+						<!--/.feature --> 
+					</div>
+					<!--/column --> 							
+				</div>
+				<!--/.row -->
+				
+				<div class="divide20"></div>
+				<div class="row wow fadeIn" data-wow-duration="1s" data-wow-delay="0.9s">
+					<div class="col-sm-6">
+						<div class="feature feature-s">
+							<div class="icon icon-s"> <img src="<?php bloginfo('template_url') ?>/resources/images/icon-05.png" alt="" /> </div>
+							<h4>Setting up and Improving Intercultural Collabaration</h4>
+							<p>You are collaborating with partners of various cultural backgrounds and different company values or are planning to start a collaboration. Neglecting the importance of cultural backgrounds and company values are the biggest obstacles for a successful partnership. </p>
+							<p>With systemic constellation we help you to understand your partners better, enabling you to improve an existing partnership or to be prepared for a planned collaboration. </p>
+						</div>
+						<!--/.feature --> 
+					</div>
+					<!--/column -->	
+					<div class="col-sm-6">
+						<div class="feature feature-s">
+							<div class="icon icon-s"> <img src="<?php bloginfo('template_url') ?>/resources/images/icon-11.png" alt="" /> </div>
+							<h4>Organisation Development</h4>
+							<p>Is your organisation more effective or adapt it to new requirements? Which changes are useful and which not</p>
+						</div>
+						<!--/.feature --> 
+					</div>
+					<!--/column -->
+				</div>
+				<!--/.row --> 
+	
+				<div class="divide60"></div>
+				<div class="row wow fadeIn" data-wow-duration="1s" data-wow-delay="1.0s">
+					<div class="col-md-4 col-md-offset-2 col-sm-12 col-xs-12 text-center wow fadeIn position-relative" data-wow-duration="1s">
+						<div class="quote style-1 bg-quote-left">
+							<blockquote>
+								<p class="quote-text">&ldquo; 
+									For problems there are the same strict ownership rules as for real estate.
+								&rdquo;</p>
+								<p>Klaus P. Horn</p>
+							</blockquote>
+						</div>
+						<figure class="img-bottom"> <img src="<?php bloginfo('template_url') ?>/resources/images/object.png" alt="" /> </figure>								
+					</div>
+					<div class="col-md-6 col-sm-12 col-xs-12">
+						<div class="divide50"></div>
+						<h2 class="text-header">Get to know each other</h2>
+						<p>If you’re still unsure. How about getting to know each other first to see if it’s right. For a free personal consulting, reach us at:</p>
+						<div class="divide10"></div>
+						<ul class="contact">
+							<li><i class="mdi mdi-phone"></i>+ 84 163 969 1059 </li>
+							<li><i class="mdi mdi-email"></i>info@consultosee.com</li>							
+						</ul>
+						 
+						<h2 class="text-header">or experience it yourself at</h2>
+						<a href="#" class="btn btn-transparent btn-large">Our workshop</a>
+					</div>
+				</div>
+				
+				<div class="divide80"></div>
+			</div>
+		</div>
+		<!-- end content-wrapper -->
+    
+<?php
+/*Main template file*/
+get_footer();
+?>
